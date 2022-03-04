7168f0e61ba5b3f43b25c9c642aa5bace10f61cb
camila-mp <ca.malvp@gmail.com>
2022-03-04 18:01:22 -0300

    remove botão dos componentes About e Projects

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export default function About() {
-  const history = useNavigate();
   return (
     <div>
       <h1>About Me</h1>
-      <button type="button" onClick={ history.goBack }>VOLTAR</button>
     </div>
   );
 }
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export default function Projects() {
-  const history = useNavigate();
   return (
     <div>
       <h1>My projects</h1>
-      <button type="button" onClick={ history.goBack }>VOLTAR</button>
     </div>
   );
 }
