eace8cf3241b7de97077911ff688acfc59fbc7d8
camila-mp <ca.malvp@gmail.com>
2022-03-04 18:00:42 -0300

    cria os elementos do componente Home.jsx e o css correspondente Home.css; adiçao de um .jpg que faz parte do Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import mypic from '../Assets/20210422_5.2.jpg';
+import './Home.css';
 
 export default function Home() {
-  const history = useNavigate();
   return (
-    <Row>
-      <Col><h1>Home</h1></Col>
-      <button type="button" onClick={ history.goBack }>VOLTAR</button>
-    </Row>
+    <Container id="home-container">
+      <img id="picture-camila" src={ mypic } alt="minha-foto" />
+      <header id="name-header">
+        <h1>Camila Malvessi Pereira</h1>
+        <h3>Desenvolvedora fullstack</h3>
+      </header>
+      <section id="info-section">
+        <h3>Habilidades</h3>
+        <div className="card-container">
+          <div className="info-card">
+            <h4>front end</h4>
+          </div>
+          <div className="info-card">
+            <h4>back end</h4>
+          </div>
+        </div>
+      </section>
+    </Container>
   );
 }
