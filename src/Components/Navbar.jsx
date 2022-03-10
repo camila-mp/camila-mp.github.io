7e8376d42129aafbe4a8f1d8b16029b5e7fc1226
camila-mp <ca.malvp@gmail.com>
2022-03-10 10:22:42 -0300

    adiciona ícones de github e linkedin à Navbar e adapta css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import './Navbar.css';
+import github from '../Assets/icons8-github-60.png';
+import linkedin from '../Assets/icons8-linkedin-60.png';
 
 export default function Navbar() {
   return (
@@ -15,6 +17,28 @@ export default function Navbar() {
         </Nav.Item>
         <Nav.Item>
           <Nav.Link className="nav-link" href="/#/projects">Projetos</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            className="nav-link"
+            id="nav-icon"
+            href="https://github.com/camila-mp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={ github } alt="link para github da Camila" />
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            className="nav-link"
+            id="nav-icon"
+            href="https://www.linkedin.com/in/camila-malvessi-pereira/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={ linkedin } alt="link para github da Camila" />
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     </Container>
