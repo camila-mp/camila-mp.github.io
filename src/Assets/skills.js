4d6acefd3c5b5764cf0f76cd3d98bd84b15cf3e4
camila-mp <ca.malvp@gmail.com>
2022-03-10 11:22:46 -0300

    estilização de Home modificada em App.css, adição de arquivo com arrays de skills

---
+++ src/Assets/skills.js
@@ -0,0 +1,11 @@
+const backendSkills = [
+  'Javascript', 'Python', 'NodeJS', 'express', 'MongoDB', 'MySQL', 'socket.io',
+  'REST APIs', 'arquitetura MSC', 'arquitetura MVC',
+];
+
+const frontendSkills = [
+  'Javascript', 'ReactJS', 'React Router', 'Redux', 'ContextAPI', 'React Hooks',
+  'Responsividade', 'RTL', 'consumo REST APIs',
+];
+
+export { backendSkills, frontendSkills };
