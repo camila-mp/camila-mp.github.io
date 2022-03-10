9a83c1ad143a0389c3934ebece80b6f61172c86c
camila-mp <ca.malvp@gmail.com>
2022-03-10 11:21:50 -0300

    adiciona  cards ao Home, modifica css para estilizar os cards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import mypic from '../Assets/20210422_5.2.jpg';
+import { backendSkills, frontendSkills } from '../Assets/skills';
 import './Home.css';
 
 export default function Home() {
+  function mapSkills(area) {
+    if (area === 'frontend') {
+      return (
+        frontendSkills
+          .map(
+            (item, index) => <div className="frontend-skill" key={ index }>{ item }</div>,
+          )
+      );
+    }
+    if (area === 'backend') {
+      return (
+        backendSkills
+          .map(
+            (item, index) => <div className="backend-skill" key={ index }>{ item }</div>,
+          )
+      );
+    }
+  }
+
   return (
     <Container id="home-container">
       <img id="picture-camila" src={ mypic } alt="minha-foto" />
       <header id="name-header">
         <h1>Camila Malvessi Pereira</h1>
-        <h3>Desenvolvedora fullstack</h3>
+        <h3 className="h3-dev-title">Desenvolvedora fullstack</h3>
       </header>
-      <section id="info-section">
-        <h3>Habilidades</h3>
-        <div className="card-container">
-          <div className="info-card">
-            <h4>front end</h4>
+      <section id="card-section">
+        <div className="card">
+          <div className="card-front">
+            <h3>front-end</h3>
           </div>
-          <div className="info-card">
-            <h4>back end</h4>
+          <div className="card-back">
+            { mapSkills('frontend') }
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-front">
+            <h3>back-end</h3>
+          </div>
+          <div className="card-back">
+            { mapSkills('backend') }
           </div>
         </div>
       </section>
