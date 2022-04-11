81479b9dec4e7bf238434bd0a4ee406641866e89
camila-mp <ca.malvp@gmail.com>
2022-04-11 16:05:38 -0300

    cria componente Projects com css

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import { toDoListVanilla, webWalletReact } from '../Assets/projects';
+import './Projects.css';
 
 export default function Projects() {
+  const projects = [toDoListVanilla, webWalletReact];
+
+  function mapCards(array) {
+    const cardGroup = array.map((item) => (
+      <Card key={ item.id } style={ { width: '18rem' } }>
+        <Card.Body>
+          <Card.Title>{ item.title }</Card.Title>
+          <Card.Text>
+            { item.summary }
+          </Card.Text>
+          <div id="proj-tag-wrap">
+            {item.tags.map(
+              (tag, index) => (
+                <div id="projectcard-tag" key={ index }>
+                  { tag }
+                </div>
+              ),
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    ));
+    return cardGroup;
+  }
+
   return (
-    <div>
+    <Container id="projects-container">
       <h1>My projects</h1>
-    </div>
+      <div id="project-cards">
+        { mapCards(projects) }
+      </div>
+    </Container>
   );
 }
