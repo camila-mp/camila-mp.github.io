41461d0a2543c18b58707beb469192646c33caa9
camila-mp <ca.malvp@gmail.com>
2022-04-12 16:03:50 -0300

    componente About + About.css + texto a ser exibido

---
+++ src/Assets/aboutMe.js
@@ -0,0 +1,19 @@
+const textAboutProfessional1 = 'Eu acredito que a tecnologia, em todas as suas vertentes,'
+  + ' tem o papel de resolver problemas e melhorar a vida das pessoas'
+  + ', e é isso que me motiva.'
+  + ' Sou bióloga e mestre em ciências biológicas,'
+  + ' mas atualmente estou em transição de carreira para desenvolvimento de software'
+  + ' web.';
+
+const textAboutProfessional2 = ' Tomei essa decisão depois de descobrir que resolver'
+  + ' problemas programando me instiga e me permite estar perto de algo que'
+  + ' gosto muito: inovação.'
+  + ' Tenho formação fullstack pela Trybe e estou em busca da minha primeira'
+  + ' oportunidade na área!';
+
+const textAboutPersonal = 'Tenho 31 anos e nasci no interior de São Paulo.'
+  + ' No meu tempo livre gosto de ler (um pouco de tudo, mas principalmente sci-fi)'
+  + ', assistir séries/filmes, codar, cuidar das minhas plantas e cuidar da minha gata.'
+  + ' Amo ciência e tecnologia.';
+
+export { textAboutProfessional1, textAboutProfessional2, textAboutPersonal };
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {
+  textAboutProfessional1,
+  textAboutProfessional2,
+  textAboutPersonal } from '../Assets/aboutMe';
+import './About.css';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <Container id="about-container">
+      <h1>Sobre mim</h1>
+      <section id="about-text-section">
+        <p>{ textAboutProfessional1 }</p>
+        <p>{ textAboutProfessional2 }</p>
+        <p>{ textAboutPersonal }</p>
+      </section>
+    </Container>
   );
 }
