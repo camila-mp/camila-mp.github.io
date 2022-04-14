36ac4056965db42333ddd2ae01e9d1537ae58a59
camila-mp <ca.malvp@gmail.com>
2022-04-14 16:12:41 -0300

    testando rotas de novo...

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={ <Navigate replace to="/home" /> } />
         <Route path="/home" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/projects" element={ <Projects /> } />
+        <Route exact path="/portfolio/about" element={ <About /> } />
+        <Route exact path="/portfolio/projects" element={ <Projects /> } />
       </Routes>
       <Footer />
     </div>
